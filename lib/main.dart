542b61aa8f1358f5fa192d8b9dc6451c5b310e81
Dongwoo Kim <dongwookim.dev@gmail.com>
2022-05-23 19:16:08 +0900

    완료버튼, 수정기능 필터기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,26 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'todo_list_notifier.dart';
 
 final todoListNotifierProvider =
-StateNotifierProvider<TodoListNotifier, List<Todo>>((ref) {
-  return TodoListNotifier();
+    StateNotifierProvider<TodoListNotifier, List<Todo>>((ref) {
+  return TodoListNotifier(const []);
+});
+
+enum TodoFilter { all, done, yet }
+
+final todoFilterProvider = StateProvider((ref) => TodoFilter.all);
+
+final filteredTodoList = Provider((ref) {
+  final todoList = ref.watch(todoListNotifierProvider);
+  final filter = ref.watch(todoFilterProvider);
+
+  switch (filter) {
+    case TodoFilter.done:
+      return todoList.where((todo) => todo.done).toList();
+    case TodoFilter.yet:
+      return todoList.where((todo) => !todo.done).toList();
+    default:
+      return todoList;
+  }
 });
 
 void main() {
@@ -39,51 +57,186 @@ class Home extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final todoList = ref.watch(todoListNotifierProvider);
+    final todoList = ref.watch(filteredTodoList);
     final textController = useTextEditingController();
-    return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
-        children: [
-          TextField(
-            controller: textController,
-            onSubmitted: (value) {
-              ref.read(todoListNotifierProvider.notifier).add(value);
-              textController.clear();
-            },
-          ),
-          const SizedBox(height: 20.0,),
-          for (var i = 0; i < todoList.length; i++) ...[
-            if(i > 0) const SizedBox(height: 10.0),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Checkbox(value: false, onChanged: (value) {}),
-                  Expanded(
-                    child: Text(
-                      todoList[i].description,
-                      style: const TextStyle(
-                        fontSize: 30.0,
-                      ),
-                    ),
-                  ),
-                  IconButton(onPressed: () {
-                    ref.read(todoListNotifierProvider.notifier).delete(todoList[i].id);
-                  }, icon: const Icon(Icons.delete))
-                ],
-              ),
-            )
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
+          children: [
+            TextField(
+              controller: textController,
+              onSubmitted: (value) {
+                ref.read(todoListNotifierProvider.notifier).add(value);
+                textController.clear();
+              },
+            ),
+            const FilterTab(),
+            const SizedBox(height: 20.0),
+            if (todoList.isEmpty)
+              const Text(
+                "Let's make new Todo!",
+                style: TextStyle(
+                  fontSize: 30.0,
+                ),
+              )
+            else
+              for (var i = 0; i < todoList.length; i++) ...[
+                if (i > 0) const SizedBox(height: 10.0),
+                ProviderScope(
+                  overrides: [
+                    _currentTodo.overrideWithValue(todoList[i]),
+                  ],
+                  child: const TodoItem(),
+                ),
+              ],
           ],
-        ],
+        ),
       ),
     );
   }
 }
 
+class FilterTab extends HookConsumerWidget {
+  const FilterTab({
+    Key? key,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final filter = ref.watch(todoFilterProvider);
 
+    Color? textColorFor(TodoFilter value) {
+      if (value == filter) {
+        return Colors.blue;
+      } else {
+        return Colors.black;
+      }
+    }
 
+    return Row(
+      children: [
+        TextButton(
+          onPressed: () {
+            ref.read(todoFilterProvider.notifier).state = TodoFilter.all;
+          },
+          style: ButtonStyle(
+            foregroundColor:
+                MaterialStateProperty.all(textColorFor(TodoFilter.all)),
+          ),
+          child: const Text('ALL'),
+        ),
+        TextButton(
+            onPressed: () {
+              ref.read(todoFilterProvider.notifier).state = TodoFilter.done;
+            },
+            style: ButtonStyle(
+              foregroundColor:
+                  MaterialStateProperty.all(textColorFor(TodoFilter.done)),
+            ),
+            child: const Text('DONE')),
+        TextButton(
+            onPressed: () {
+              ref.read(todoFilterProvider.notifier).state = TodoFilter.yet;
+            },
+            style: ButtonStyle(
+              foregroundColor:
+                  MaterialStateProperty.all(textColorFor(TodoFilter.yet)),
+            ),
+            child: const Text('YET')),
+      ],
+    );
+  }
+}
+
+final _currentTodo = Provider<Todo>((ref) => throw UnimplementedError());
+
+class TodoItem extends HookConsumerWidget {
+  const TodoItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todo = ref.watch(_currentTodo);
+    final itemFocusNode = useFocusNode();
+    final itemIsFocus = useIsFocused(itemFocusNode);
+
+    final textEditingController = useTextEditingController();
+    final textFieldFocusNode = useFocusNode();
+
+    return Focus(
+      focusNode: itemFocusNode,
+      onFocusChange: (focused) {
+        if (!focused) {
+          textEditingController.text = todo.description;
+        } else {
+          ref
+              .read(todoListNotifierProvider.notifier)
+              .edit(id: todo.id, desc: textEditingController.text);
+        }
+      },
+      child: ListTile(
+        onTap: () {
+          itemFocusNode.requestFocus();
+          textFieldFocusNode.requestFocus();
+        },
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Checkbox(
+                value: todo.done,
+                onChanged: (value) {
+                  ref.read(todoListNotifierProvider.notifier).done(todo.id);
+                }),
+            const SizedBox(width: 20.0),
+            Expanded(
+              child: itemIsFocus
+                  ? TextField(
+                      autofocus: true,
+                      focusNode: textFieldFocusNode,
+                      onSubmitted: (value) {
+                        ref
+                            .read(todoListNotifierProvider.notifier)
+                            .edit(id: todo.id, desc: value);
+                      },
+                      style: const TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    )
+                  : Text(
+                      todo.description,
+                      style: const TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+            ),
+            IconButton(
+                onPressed: () {
+                  ref.read(todoListNotifierProvider.notifier).delete(todo.id);
+                },
+                icon: const Icon(Icons.delete))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+bool useIsFocused(FocusNode node) {
+  final isFocused = useState(node.hasFocus);
+
+  useEffect(() {
+    void listener() {
+      isFocused.value = node.hasFocus;
+    }
+
+    node.addListener(listener);
+    return () {
+      node.removeListener(listener);
+    };
+  }, [node]);
+
+  return isFocused.value;
+}
--- lib/todo_list_notifier.dart
+++ lib/todo_list_notifier.dart
@@ -8,28 +8,62 @@ const _uuid = Uuid();
 class Todo {
   final String id;
   final String description;
+  final bool done;
 
   const Todo({
     required this.id,
     required this.description,
+    required this.done,
   });
 }
 
 class TodoListNotifier extends StateNotifier<List<Todo>> {
-  TodoListNotifier([List<Todo>? initialTodos]) : super(initialTodos ?? []);
+  TodoListNotifier(List<Todo>? initialTodos) : super(initialTodos ?? []);
 
   void add(String description) {
     state = [
       ...state,
-      Todo(id: _uuid.v4(), description: description),
+      Todo(
+        id: _uuid.v4(),
+        description: description,
+        done: false,
+      ),
     ];
   }
 
   void delete(String id) {
     state = [
-      for (var todo in state)
-        if (todo.id != id)
+      for (final todo in state)
+        if (todo.id != id) todo
+    ];
+  }
+
+  void edit({required String id, required String desc}) {
+    print('this is state ${state[0].id}, $id');
+    state = [
+      for (final todo in state)
+        if (todo.id == id)
+          Todo(
+            id: todo.id,
+            description: desc,
+            done: todo.done,
+          )
+        else
           todo
+    ];
+  }
+
+  void done(String id) {
+    state = [
+      for (final todo in state)
+        if (todo.id == id)
+          Todo(
+            id: todo.id,
+            description: todo.description,
+            done: !todo.done,
+          )
+      else
+        todo
     ];
   }
 }
